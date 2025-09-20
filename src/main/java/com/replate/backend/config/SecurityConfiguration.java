3c16579b254fc76d3f26aa60940d809d85d4d4e0
liviu5m <motpanliviuwork@gmail.com>
2025-09-20 10:30:03 +0300

    fixed security endpoint

--- src/main/java/com/replate/backend/config/SecurityConfiguration.java
+++ src/main/java/com/replate/backend/config/SecurityConfiguration.java
@@ -57,7 +57,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://replateapp.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET","POST", "DELETE", "PUT"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
